374bc48ac72273f0ffbe26a6616d845cf322da01
pandiyan <pandiyanfsd@gmail.com>
2026-05-29 20:36:42 +0530

    running complete pro =ject in vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000 // increase limit to 2 MB
+  },
   server: {
     proxy: {
       '/api': {
