690ce0068212ebd2e51eb021e1f6b2b7b8cf2854
pandiyan <pandiyanfsd@gmail.com>
2026-08-08 14:55:47 +0530

    Fix Vercel build failure by removing C++ packages from client and cleaning vercel.json

--- client/api/index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-try {
-  require('dotenv').config({ path: path.join(__dirname, '../../server/.env') });
-} catch (e) {}
-
-const app = require('../../server/index');
-
-module.exports = app;
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
