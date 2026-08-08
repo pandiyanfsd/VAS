8410d1c846d2c2f538a08ea74c84ce55f18497f3
pandiyan <pandiyanfsd@gmail.com>
2026-08-08 14:20:24 +0530

    Fix vercel.json destination paths with leading slashes

--- api/index.js
+++ api/index.js
@@ -5,9 +5,4 @@ try {
 
 const app = require('../server/index');
 
-module.exports = (req, res) => {
-  if (req.headers['x-matched-path']) {
-    req.url = req.headers['x-matched-path'];
-  }
-  return app(req, res);
-};
+module.exports = app;
